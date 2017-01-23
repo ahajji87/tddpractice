9c1af465037f862bb1b0041bb6f89e2b8dd4d85d
Amine Hajji <ahajji@magento.com>
2017-01-23 10:58:59 +0100

    add unit tests

---
+++ tests/EncriptadorTest.php
@@ -0,0 +1,108 @@
+<?php
+
+
+class EncriptadorTest extends \PHPUnit\Framework\TestCase
+{
+    /** @var  \App\Classes\Encriptador */
+    private $encriptor;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->encriptor = new \App\Classes\Encriptador();
+    }
+
+    /**
+     * @expectedException Exception
+     */
+    public function testEncriptadorMethodCryptWordShouldThrowExceptionWhenFoundSpace()
+    {
+        $word = "hola mundo";
+
+        $this->encriptor->cryptWord($word);
+    }
+
+    public function testEncriptadorMethodCryptWordShouldCryptGivenWord()
+    {
+        $word = 'holamundo';
+
+        $this->assertEquals('jqncowpfq', $this->encriptor->cryptWord($word));
+    }
+
+    /**
+     * @expectedException Exception
+     */
+    public function testEncriptadorMethodCryptWordToNumbersShouldThrowExceptionWhenFoundSpace()
+    {
+        $word = "hola mundo";
+
+        $this->encriptor->cryptWordToNumbers($word);
+    }
+
+    public function testEncriptadorMethodCryptWordToNumbersShouldCryptGivenWord()
+    {
+        $word = 'holamundo';
+
+        $this->assertEquals('10611311099111119112102113', $this->encriptor->cryptWordToNumbers($word));
+    }
+
+    public function testEncriptadorMethodCryptSentenceShouldCryptGivenSentence()
+    {
+        $word = 'soy una frase';
+
+        $this->assertEquals('uq{"wpc"htcug', $this->encriptor->cryptSentence($word));
+    }
+
+    /**
+     * @expectedException Exception
+     */
+    public function testEncriptadorMethodCryptWordPartiallyShouldThrowExceptionWhenFoundSpace()
+    {
+        $word = "hola mundo";
+
+        $this->encriptor->cryptWordPartially($word, 'a');
+    }
+
+    public function testEncriptadorMethodCryptWordPartiallyShouldReturnNullWhenCharsToReplaceIsNull()
+    {
+        $word = "holamundo";
+
+        $this->assertEquals('', $this->encriptor->cryptWordPartially($word, ''));
+    }
+
+    public function testEncriptadorMethodCryptWordPartiallyShouldNotCryptWhenCharsToReplaceIsNotMatched()
+    {
+        $word = "holamundo";
+
+        $this->assertEquals('holamundo', $this->encriptor->cryptWordPartially($word, 'x'));
+    }
+
+    public function testEncriptadorMethodCryptWordPartiallyShouldCryptWhenCharsToReplaceIsMatch()
+    {
+        $word = "holamundo";
+
+        $this->assertEquals('jolamundo', $this->encriptor->cryptWordPartially($word, 'h'));
+    }
+
+    public function testEncriptadorMethodGetWordsShouldReturnArrayOfWordsFromGivenSentence()
+    {
+        $sentence = "hola mundo mundial";
+
+        $array = ["hola", "mundo", "mundial"];
+
+        $this->assertEquals($array, $this->encriptor->getWords($sentence));
+    }
+
+    public function testEncriptadorMethodPrintWordsShouldPrintWordsFromGivenSentence()
+    {
+        $sentence = "hola mundo mundial";
+
+        $output = '<hola><mundo><mundial>';
+
+        $this->encriptor->printWords($sentence);
+        $this->expectOutputString($output);
+    }
+
+
+}
